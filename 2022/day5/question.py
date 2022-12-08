d5302cf40b83d888b00f9d7549531fc74f437e89
Petros Xenofontos <petros.x12@gmail.com>
2022-12-08 18:00:12 +0000

    day 5 # working version on test input

--- 2022/day5/question.py
+++ 2022/day5/question.py
@@ -16,20 +16,32 @@ def parse_expression(expression):
 def move_crates(soc, number_of_crates, origin_stack_id, destination_stack_id):
 
     origin_stack = soc[origin_stack_id]
-    origin_stack_cut = origin_stack[:-number_of_crates]
+    origin_stack_cut = origin_stack[-number_of_crates:]
+
+    print(f"Cut: {origin_stack_cut}")
 
     # reversal happens in place to simulate movement of one crate at a time
     origin_stack_cut.reverse()
 
     for crate in origin_stack_cut:
         soc[destination_stack_id].append(crate)
+        
+    print(soc[destination_stack_id])
 
     new_origin_stack = soc[origin_stack_id][:(len(origin_stack)-number_of_crates)]
+
+    print(new_origin_stack)
 
     soc[origin_stack_id] = new_origin_stack
 
     return soc
 
+
+def reordered_crates(stack_of_crates, command):
+    movements = parse_expression(command.strip())
+
+    stack_of_crates = move_crates(soc=stack_of_crates, number_of_crates=movements[0], origin_stack_id=movements[1],destination_stack_id= movements[2])
+    return stack_of_crates
 
 if __name__ == "__main__":
     stack_of_crates = {
@@ -44,7 +56,13 @@ if __name__ == "__main__":
         9: ['R','D','G','C','P','B','Q','M'],
     }
 
-    # print(stack_of_crates)
+    # test_input = {
+    #     1: ['Z','N'],
+    #     2: ['M','C','D'],
+    #     3: ['P'],
+    # }
+
+    print(stack_of_crates)
     read_flag = 0
     counter_flag = 0
 
@@ -57,14 +75,23 @@ if __name__ == "__main__":
             #     counter_flag+=1
             # if read_flag == 1:
             print((index/length_lines)*100)
-            movements = parse_expression(line.strip())
-            stack_of_crates = move_crates(soc=stack_of_crates, number_of_crates=movements[0], origin_stack_id=movements[1],destination_stack_id= movements[2])
+
+            stack_of_crates = reordered_crates(stack_of_crates, line)
 
             print(line)
             # print(stack_of_crates)
 
             # if line == '\n':
             #     read_flag = 1        
-    
-    for x in stack_of_crates.items():
-        print(f"{x[0]}, {x[1]}")
+
+    # with open("test_input.txt") as f:
+    #     all_lines = f.readlines()
+    #     for index,line in enumerate(all_lines):
+    #         print(f"=={line.strip()}=====")
+    #         test_input = reordered_crates(test_input, line)
+
+    # for x in stack_of_crates.items():
+    #     print(f"{x[0]}, {x[1]}")
+
+    # for x in test_input.items():
+    #     print(f"{x[0]}, {x[1]}")
