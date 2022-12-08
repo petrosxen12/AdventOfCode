663bbf657eeffb8784dcac549b35950b8063de3e
Petros Xenofontos <petros.x12@gmail.com>
2022-12-08 23:49:25 +0000

    day 6 # finishes part two

--- 2022/day6/question.py
+++ 2022/day6/question.py
@@ -2,9 +2,11 @@
 your subroutine needs to identify the first position where the four most recently received characters were all different. 
 Specifically, it needs to report the number of characters from the beginning of the buffer to the end of the first such four-character marker.
 """
+DISTINCT_CHARACTER_SELECTION = 14
+
 def is_unique(window):
     # print(window)
-    if len(set(window)) < 4:
+    if len(set(window)) < DISTINCT_CHARACTER_SELECTION:
         return False
 
     return True
@@ -13,8 +15,8 @@ if __name__ == "__main__":
    
     with open("input.txt", encoding="utf-8") as f:
         data_stream = f.readline()
-        # test_data_stream = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
-        search_window = 4
+        # data_stream = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+        search_window = DISTINCT_CHARACTER_SELECTION
         # print(data_stream)
         for index, character in enumerate(data_stream):
             chars_until_uniqueness = search_window+index
